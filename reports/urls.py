f4ec0101d9d59b0e8b3198f7f0d6122057e519b5
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-10-03 00:15:05 +0200

    Create safety report page creation

--- reports/urls.py
+++ reports/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('about/', views.about, name='about'),
+    path('create/', views.create_report, name='create_report'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'reports/about.html')
+
+
+def create_report(request):
+    return render(request, 'reports/create_report.html')

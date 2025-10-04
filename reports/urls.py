aacd054baf34ecb2c5caa4a0d48bda8073a3bbfa
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-10-04 17:03:17 +0200

    Addition of Report Detail pages, accessed by click on the summary report card on the board page

--- reports/urls.py
+++ reports/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('board/', views.board, name='board'),
+    path('report/<int:pk>/', views.report_detail, name='report_detail'),
     path('create/', views.create_report, name='create_report'),
 ]

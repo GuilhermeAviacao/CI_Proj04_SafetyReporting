04aae059f036b9cb9a545f445e3ed7256675fb0f
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-10-04 22:37:44 +0200

    Ability for Role: Investigators to change Report Status

--- reports/urls.py
+++ reports/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('board/', views.board, name='board'),
     path('report/<int:pk>/', views.report_detail, name='report_detail'),
+    path('report/<int:pk>/update-status/', views.update_investigation_status, name='update_investigation_status'),
     path('create/', views.create_report, name='create_report'),
 ]

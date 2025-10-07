c962b0485b780e4682a8244c047b46576eadca61
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-10-07 12:58:36 +0200

    Heroku deploy test

--- aviation_safety/settings.py
+++ aviation_safety/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 import os
 import dj_database_url
 from decouple import config
-import env
+
+# Import env.py if it exists
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
